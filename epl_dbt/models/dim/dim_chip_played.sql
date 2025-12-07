837c959a7d5f0cddea21d990cf2eee402604b2b3
majay777 <ajaymugle87@gmail.com>
2025-12-07 15:36:46 +0530

    Made changes to dim_chip_played.sql and dim_gameweek_info.sql to get the current gameweek info.

--- epl_dbt/models/dim/dim_chip_played.sql
+++ epl_dbt/models/dim/dim_chip_played.sql
@@ -2,7 +2,7 @@ with c as (
     select * from {{ ref("src_chip_plays")}}  QUALIFY row_number() over (partition by sid  order by sid) = 1
 ),
 e as (
-    select * from {{ ref("dim_events") }}   QUALIFY row_number() over (partition by sid order by sid) = 1
+    select * from {{ ref("dim_events")  }}  where finished=True or is_current=True  QUALIFY row_number() over (partition by sid order by sid) = 1
 )
 
 select  chip_name, num_played, e.id, e.name,
@@ -11,4 +11,4 @@ select  chip_name, num_played, e.id, e.name,
                 month(e.deadline_time) in (1,2,3,4,5) then 
                 (year(e.deadline_time) - 1):: varchar || '-' ||  year(e.deadline_time):: varchar end as Season from e)) as Season
                 from   c
-                right join  e on c._dlt_parent_id = e._dlt_id where e.finished = 'True'  order by id
+                right join  e on c._dlt_parent_id = e._dlt_id where e.finished = True or is_current=True order by id
--- epl_dbt/models/dim/dim_gameweek_info.sql
+++ epl_dbt/models/dim/dim_gameweek_info.sql
@@ -19,7 +19,7 @@ m as (
 
 
 
-select e.id, name, average_entry_score as Average_Points, highest_score as Highest_Points,
+select distinct(e.id) as id, name, average_entry_score as Average_Points, highest_score as Highest_Points,
                top_element_info__points as Points, transfers_made as Gameweek_Transfers,
                 el.web_name as Most_Transferred_In, el1.web_name as
                 Most_Selected,el2.web_name as Most_Captained, el3.web_name as Most_Points,
@@ -31,4 +31,4 @@ select e.id, name, average_entry_score as Average_Points, highest_score as Highe
                e join  el on e.most_transferred_in = el.id join  el1 on e.most_selected
                 = el1.id join  el2 on e.most_captained = el2.id join  el3 on
                  e.top_element = el3.id
-                where finished=True order by name
+                where e.finished=True or e.is_current=True order by name
